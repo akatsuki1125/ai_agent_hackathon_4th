522c58d366dd84faf468f4d6d4d79ff4632e68f0
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2025-12-22 14:18:52 +0900

    send_packet dual method

--- tcp_udp_experiment/udp_tcp_exp/tcp_client.py
+++ tcp_udp_experiment/udp_tcp_exp/tcp_client.py
@@ -2,18 +2,21 @@ import socket
 
 #SERVER_ADDR = "192.168.0.12" ローカルホストじゃないと多分無理っぽい？
 SERVER_ADDR = "127.0.0.1"
-#PORT = "40" #特権ポートなのでだめ（1 ~ 1023)，あと，数字で指定しよう
+#PORT = "40" #特権ポートなのでだめ（1 ~ 1023)
 PORT = 8008
 
 # socket の方か，connection の方かどっちかだけを使う，今回は connection だけでオッケー
 skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("start connection_establish")
 #connection = socket.create_connection(SERVER_ADDR, PORT) タプルで指定する必要あり
-connection = socket.create_connection((SERVER_ADDR, PORT))
+#connection = socket.create_connection((SERVER_ADDR, PORT)) #socket を使わん場合これでオッケー
+connection = skt.connect((SERVER_ADDR, PORT))
+
 print("finish connection_establish")
 
 datasize = 100
 data = b"\x00" * datasize
 
 print("send_packet")
-connection.sendall(data)
+#connection.sendall(data)
+skt.sendall(data)
--- tcp_udp_experiment/udp_tcp_exp/tcp_server.py
+++ tcp_udp_experiment/udp_tcp_exp/tcp_server.py
@@ -4,11 +4,12 @@ import socket
 SERVER_ADDR = "127.0.0.1"
 PORT = 8008
 
-skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-skt.bind((SERVER_ADDR, PORT))
+#skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#skt.bind((SERVER_ADDR, PORT))
 
-skt.listen(4)
+#skt.listen(4)
 #skt.accept()
+skt = socket.create_server((SERVER_ADDR, PORT))
 conn, addr = skt.accept()
 buf = conn.recv(1024)
 
