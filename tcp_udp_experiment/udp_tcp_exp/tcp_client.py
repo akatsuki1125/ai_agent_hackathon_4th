21cf68e77c7ea0c9fe8cc816dbefc00f47b783d7
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2025-12-22 13:36:23 +0900

    my_client_failed

--- tcp_udp_experiment/udp_tcp_exp/tcp_client.py
+++ tcp_udp_experiment/udp_tcp_exp/tcp_client.py
@@ -2,6 +2,7 @@ import socket
 
 SERVER_ADDR = "192.168.0.12"
 
-skt = socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.create_connection(SERVER_ADDR)
+skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connection = socket.create_connection(SERVER_ADDR)
 
+socket.sendall(skt)
