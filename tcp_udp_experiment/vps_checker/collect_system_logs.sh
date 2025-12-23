c887dd3108bb09d72d32937f213306162d291fb3
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2025-12-23 13:56:44 +0900

    add: storage and network monitor

--- tcp_udp_experiment/vps_checker/collect_system_logs.sh
+++ tcp_udp_experiment/vps_checker/collect_system_logs.sh
@@ -9,10 +9,23 @@ OUTDIR_SYSTEMD_RUNNING="$BASE/systemd/running/$DATE"
 OUTDIR_SYSTEMD_ENABLED="$BASE/systemd/enabled/$DATE"
 OUTDIR_APT_MANUAL="$BASE/apt/manual/$DATE"
 
+# network
+OUTDIR_NET_SOCKETS="$BASE/network/sockets/$DATE"
+OUTDIR_NET_INTERFACES="$BASE/network/interfaces/$DATE"
+OUTDIR_NET_ROUTES="$BASE/network/routes/$DATE"
+OUTDIR_NET_STATS="$BASE/network/stats/$DATE"
+
+OUTDIR_STORAGE_DF="$BASE/storage/df/$DATE"
+
 mkdir -p \
   "$OUTDIR_SYSTEMD_RUNNING" \
   "$OUTDIR_SYSTEMD_ENABLED" \
-  "$OUTDIR_APT_MANUAL"
+  "$OUTDIR_APT_MANUAL" \
+  "$OUTDIR_NET_SOCKETS" \
+  "$OUTDIR_NET_INTERFACES" \
+  "$OUTDIR_NET_ROUTES" \
+  "$OUTDIR_NET_STATS" \
+  "$OUTDIR_STORAGE_DF"
 
 # systemd: running services
 systemctl list-units --type=service --state=running \
@@ -25,3 +38,28 @@ systemctl list-unit-files --state=enabled \
 # apt: manual packages
 apt-mark showmanual | sort \
   > "$OUTDIR_APT_MANUAL/$TIME.txt"
+
+# sockets
+ss -tunap \
+  > "$OUTDIR_NET_SOCKETS/$TIME.txt"
+
+# interfaces
+ip addr \
+  > "$OUTDIR_NET_INTERFACES/$TIME.txt"
+
+# routes
+ip route \
+  > "$OUTDIR_NET_ROUTES/$TIME.txt"
+
+# kernel stats
+{
+  echo "### /proc/net/snmp"
+  cat /proc/net/snmp
+  echo
+  echo "### /proc/net/netstat"
+  cat /proc/net/netstat
+} > "$OUTDIR_NET_STATS/$TIME.txt"
+
+df -h > "$OUTDIR_STORAGE_DF/$TIME.txt"
+df -i > "$OUTDIR_STORAGE_DF/$TIME.txt"
+
--- tcp_udp_experiment/vps_checker/diff_daily.sh
+++ tcp_udp_experiment/vps_checker/diff_daily.sh
@@ -14,6 +14,18 @@ ENABLED_T="$BASE/systemd/enabled/$TODAY/0200.txt"
 RUNNING_Y="$BASE/systemd/running/$YESTERDAY/0200.txt"
 RUNNING_T="$BASE/systemd/running/$TODAY/0200.txt"
 
+# network（interfaces/routes は自動diff向き）
+NET_IF_Y="$BASE/network/interfaces/$YESTERDAY/0200.txt"
+NET_IF_T="$BASE/network/interfaces/$TODAY/0200.txt"
+NET_ROUTE_Y="$BASE/network/routes/$YESTERDAY/0200.txt"
+NET_ROUTE_T="$BASE/network/routes/$TODAY/0200.txt"
+
+# storage（df / df-i は事故検知に強い）
+DF_Y="$BASE/storage/df/$YESTERDAY/0200.txt"
+DF_T="$BASE/storage/df/$TODAY/0200.txt"
+DFI_Y="$BASE/storage/df-i/$YESTERDAY/0200.txt"
+DFI_T="$BASE/storage/df-i/$TODAY/0200.txt"
+
 mkdir -p "$DIFFDIR"
 
 diff_one () {
@@ -42,3 +54,8 @@ diff_one () {
 diff_one "systemd-enabled" "$ENABLED_Y" "$ENABLED_T"
 diff_one "systemd-running" "$RUNNING_Y" "$RUNNING_T"
 
+diff_one "net-interfaces" "$NET_IF_Y" "$NET_IF_T"
+diff_one "net-routes" "$NET_ROUTE_Y" "$NET_ROUTE_T"
+
+diff_one "storage-df" "$DF_Y" "$DF_T"
+diff_one "storage-df-i" "$DFI_Y" "$DFI_T"
