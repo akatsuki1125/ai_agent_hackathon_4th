3dfc9164bb4783336d70af48d49ee6ac23bb9c04
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2025-12-25 19:58:55 +0900

    feat: dump_extractor_implemented

--- tcp_udp_experiment/vps_checker/dump_analysis.py
+++ tcp_udp_experiment/vps_checker/dump_analysis.py
@@ -3,70 +3,49 @@ import re
 
 dump_dir = "net_data"
 dump_files = os.listdir(dump_dir)
-#print(dump_files)
-#['ssh_22_full.log', 'tcp_syn_all.log', 'icmp.log', 'http_80.log']
-
 file_path = os.path.join(dump_dir, dump_files[0])
-# with open(file_path, 'r') as f:
-#     for line in f:
-#         print(line)
-#         break
+
+read_line_size = 5
 
 data = []
 with open(file_path, 'r') as f:
-    for i in range(5):
-        #print(f.readline().rstrip())
+    for i in range(read_line_size):
         data.append(f.readline().rstrip())
-
-#data = [d.split(' ') for d in data]
-#print(data[0])
-
-
-date_reg = r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6})'
-data_sep = []
-for line in data:
-    m = re.match(date_reg + r"\s+(.*)", line)
-    if m:
-        data_sep.append([m.group(1),m.group(2)])
-
-#print(data_sep)
-data = data_sep
-data_sep = []
-
-ip_reg = r'IP (\S+) > (\S+):'
-for line in data:
-    m = re.match(ip_reg + r"\s+(.*)", line[1])
-    if m:
-        data_sep.append([line[0], m.group(1),m.group(2),m.group(3)])
         
-print(data_sep)
-
-fields = ['Flags', 'seq', 'ack', 'win', 'options', 'length']
+date_reg = r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6})'
+ip_reg = r'IP (\S+) > (\S+):'
 val_reg = r'(\d+(?::\d+)?)'
 val_regs = {"seq": r"seq "+val_reg, "ack": r"ack "+val_reg, "win": r"win "+val_reg, "length": r"length "+val_reg}
 flag_reg = r'Flags (\[[^\]]*\])'
 options_reg = r'options (\[[^\]]*\])'
 
-data = data_sep
-records = []
-record = {}
 
-def extract(reg, str):
+def extract(reg, text, field=1):
     m = re.search(reg, str)
     if m:
-        return m.group(1)
+        return m.group(field)
     else:
-        return None
+        return ''
+
+def split_ip_port(addr):
+    if addr.count('.') == 4:
+        return addr.rsplit('.')
+    else:
+        return addr, ''
+
+records = []
 
 for line in data:
-    record['date'] = line[0]
-    record['src_ip'] = line[1]
-    record['dst_ip'] = line[2]
-    record['flag'] = extract(flag_reg, line[3])
-    record['options'] = extract(options_reg, line[3])
+    record = {}
+    record['date'] = extract(date_reg, line)
+    record['src_ip'], record['src_port'] = split_ip_port(extract(ip_reg, line, 1))
+    record['dst_ip'], record['dst_port'] = split_ip_port(extract(ip_reg, line, 2))
+    record['flag'] = extract(flag_reg, line)
+    record['options'] = extract(options_reg, line)
     for k,v in val_regs.items():
-        record[k] = extract(v, line[3])
+        record[k] = extract(v, line)
         
     records.append(record)
+
 print(records)
 
