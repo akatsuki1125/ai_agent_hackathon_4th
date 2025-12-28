7f1aa982bea9ec39f7fb3d3035cda481a1042f82
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2025-12-28 13:54:24 +0900

    add: plot_funcs

--- tcp_udp_experiment/vps_checker/dump_analysis.py
+++ tcp_udp_experiment/vps_checker/dump_analysis.py
@@ -1,16 +1,20 @@
 import os
 import re
+from itertools import islice
+from matplotlib import pyplot as plt
+from datetime import datetime
+from collections import Counter, defaultdict
 
-dump_dir = "net_data"
-dump_files = os.listdir(dump_dir)
-file_path = os.path.join(dump_dir, dump_files[0])
+dump_dir = "net_data_20251226_1904"
+dump_files = [f for f in os.listdir(dump_dir) if os.path.isfile(os.path.join(dump_dir, f))]
+dump_file = dump_files[3]
+file_path = os.path.join(dump_dir, dump_file)
 
 read_line_size = None
 
 data = []
 with open(file_path, 'r') as f:
-    for i in range(read_line_size):
-        data.append(f.readline().rstrip())
+    data = list(islice(f, read_line_size))
         
 date_reg = r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6})'
 ip_reg = r'IP (\S+) > (\S+):'
@@ -18,7 +22,6 @@ val_reg = r'(\d+(?::\d+)?)'
 val_regs = {"seq": r"seq "+val_reg, "ack": r"ack "+val_reg, "win": r"win "+val_reg, "length": r"length "+val_reg}
 flag_reg = r'Flags (\[[^\]]*\])'
 options_reg = r'options (\[[^\]]*\])'
-
 
 def extract(reg, text, field=1):
     m = re.search(reg, text)
@@ -47,5 +50,29 @@ for line in data:
         
     records.append(record)
 
-print(records)
+src_ips = [r['src_ip'] for r in records]
+counts = Counter(src_ips)
+top10_ip, _ = zip(*sorted(counts.items(), key=lambda x:x[1], reverse=True)[:10])
 
+counts_by_ip = defaultdict(Counter)
+for r in records:
+    ip = r["src_ip"]
+    if r["date"]:
+        date_mins = datetime.strptime(r["date"], "%Y-%m-%d %H:%M:%S.%f").replace(second=0, microsecond=0) 
+    counts_by_ip[ip][date_mins] += 1
+
+# dates = [r['date'] for r in records if r['src_ip'] in top10_ip]
+# date_mins = [datetime.strptime(r["date"], "%Y-%m-%d %H:%M:%S.%f").replace(second=0, microsecond=0) for date in dates]
+# counts = Counter(date_mins)
+# xs, ys = zip(*sorted(counts.items()))
+
+plt.title(dump_file)
+for ip in top10_ip:
+    xs, ys = zip(*sorted(counts_by_ip[ip].items()))
+    plt.plot(xs, ys,label=ip)
+
+plt.legend()
+plt.xticks(rotation=45)
+plt.tight_layout()
+os.makedirs(f'{dump_dir}/plots', exist_ok=True)
+plt.savefig(f'{dump_dir}/plots/{dump_file}.png')
